c1d2a76530c3fe9e6cd5169676aeca5ba2249973
DSPandey <dhirajpandey37@gmail.com>
2026-07-20 00:47:19 +0530

    chore: initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,60 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
+
+    rules: {
+      // Imports
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+
+      "unused-imports/no-unused-imports": "error",
+
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      // Console
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+
+      // React
+      "react/no-unescaped-entities": "off",
+    },
+  },
+
+  eslintConfigPrettier,
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "coverage/**",
+    "node_modules/**",
+    "dist/**",
+    "sanity.types.ts",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
